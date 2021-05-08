057809e70fbc514ec1323adfa30af7fd5ec47fc7
xinwang <1294600058@qq.com>
2021-05-09 03:17:45 +0800

    ADD: 增加 html 模板

--- src/layouts/component/Header/index.tsx
+++ src/layouts/component/Header/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import {
+  UserOutlined,
+  QuestionCircleOutlined,
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 import styles from './index.css';
+const { SubMenu, Item } = Menu;
 
 const Header: React.FC = props => {
-  console.log(props);
+  const [currentIcon, setCurrentIcon] = useState('mail');
+  const handleClick = e => {
+    console.log('click ', e);
+    setCurrentIcon(e.key);
+  };
   return (
     <div className={styles.header}>
       <div className={styles['header-left']}>
-        <span className={styles['header-left-text']}>
+        <div className={styles['header-left-text']}>
           <a className={styles.logo} href="http://49.234.210.170/">
             XINWANGBLOG
-          </a>{' '}
-        </span>
+          </a>
+        </div>
         <span className={styles['header-left-text']}></span>
       </div>
       <div className="header-right">
