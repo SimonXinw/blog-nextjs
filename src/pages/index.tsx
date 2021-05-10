4b44e3e83ffb0edb6b972e702df857f1fed207cc
xinwang <wb-xw818042@alibaba-inc.com>
2021-05-10 11:27:07 +0800

    Add: 增加首页icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FireFilled } from '@ant-design/icons';
 import styles from './index.css';
 import { randomColor } from '@/utils';
 
@@ -45,7 +46,7 @@ const BlogUrlList = [
   },
   {
     url: 'https://blog.csdn.net/qq_42877027/article/details/104959936',
-    name: '放大镜',
+    name: 'node开启Http',
   },
   {
     url: 'http://49.234.210.170/js/购物车/todopst/todo.html',
@@ -74,6 +75,27 @@ const BlogUrlList = [
   {
     url: 'http://49.234.210.170/js/排序算法.html',
     name: '排序算法',
+    icon: (
+      <svg fill="currentColor" viewBox="0 0 24 24" width="18" height="18">
+        <defs>
+          <linearGradient
+            id="id-2014200654-a"
+            x1="63.313%"
+            x2="46.604%"
+            y1="-13.472%"
+            y2="117.368%"
+          >
+            <stop offset="2.35%" stop-color="#EC471E"></stop>
+            <stop offset="100%" stop-color="#FF6DC4"></stop>
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#id-2014200654-a)"
+          d="M14.553 20.78c.862-.651 1.39-1.792 1.583-3.421.298-2.511-.656-4.904-2.863-7.179.209 2.291.209 3.73 0 4.314-.41 1.143-1.123 1.983-1.91 2.03-1.35.079-2.305-.512-2.863-1.774-.676 1.25-.782 2.556-.318 3.915.31.906.94 1.684 1.89 2.333C7.144 20.131 5 17.336 5 14.022c0-2.144.898-4.072 2.325-5.4.062 2.072.682 3.598 2.13 4.822-.67-1.112-.734-2.11-.734-3.517 0-3.253 2.067-6.007 4.913-6.927a7.35 7.35 0 0 0 2.157 4.918C17.722 9.214 19 11.463 19 14.022c0 3.073-1.844 5.7-4.447 6.758z"
+          fill-rule="evenodd"
+        ></path>
+      </svg>
+    ),
   },
   {
     url: 'http://49.234.210.170/js/双飞翼布局.html',
@@ -82,6 +104,7 @@ const BlogUrlList = [
   {
     url: 'http://49.234.210.170/js/复利计算.html',
     name: '复利计算',
+    icon: <FireFilled style={{ color: '#FF4500' }} />,
   },
 
   {
@@ -124,6 +147,7 @@ export default function() {
               <a target="_blank" href={item.url} rel="noreferrer">
                 {item.name}
               </a>
+              {item.icon}
             </p>
           );
         })}
