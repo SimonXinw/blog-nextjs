a66bd234d7f4c9e5dc2111489862c1e3b52ca09f
wb-xw818042 <wb-xw818042@alibaba-inc.com>
2023-04-11 15:16:56 +0800

    feat: 封面第一帧图片

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -250,3 +250,28 @@ export function linearMotion(config: AutoReboundTs) {
 
   // END
 }
+
+/**
+ * @计算视频第一帧
+ */
+export const getVideoBase64 = (url: string) => {
+  return new Promise(function (resolve) {
+    let dataURL = '';
+    let video = document.createElement('video');
+    video.setAttribute('crossOrigin', 'anonymous'); //处理跨域
+    video.setAttribute('src', url);
+    video.setAttribute('width', '400');
+    video.setAttribute('height', '400');
+    video.setAttribute('preload', 'auto');
+    video.addEventListener('loadeddata', function () {
+      let canvas: any = document.createElement('canvas'),
+        width = video.width, //canvas的尺寸和图片一样
+        height = video.height;
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext('2d').drawImage(video, 0, 0, width, height); //绘制canvas
+      dataURL = canvas?.toDataURL('image/jpeg') as string; //转换为base64
+      resolve(dataURL);
+    });
+  });
+};
