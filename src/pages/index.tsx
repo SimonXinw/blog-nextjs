00fb08de5f9924c2e34075f6c55b8da3ec339cd9
辛望 <1294600058@qq.com>
2023-04-17 22:02:15 +0800

    feat: 测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,116 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import { FireFilled } from '@ant-design/icons';
-import styles from '@/styles/Home.module.css';
-import { randomColor, linearMotion } from '@/utils';
-import RecordNumber from '@/components/RecordNumber';
-import VideoBG from '@/components/VideoBG';
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { FireFilled } from "@ant-design/icons";
+import styles from "@/styles/Home.module.css";
+import { randomColor, linearMotion } from "@/utils";
+import RecordNumber from "@/components/RecordNumber";
+import VideoBG from "@/components/VideoBG";
 
 const BlogUrlList = [
   {
-    url: 'http://49.234.210.170/js/缓冲运动.html',
-    name: '缓冲运动',
+    url: "http://49.234.210.170/js/缓冲运动.html",
+    name: "缓冲运动",
   },
   {
-    url: 'http://49.234.210.170/js/缓冲运动碰壁.html',
-    name: '缓冲运动碰壁',
+    url: "http://49.234.210.170/js/缓冲运动碰壁.html",
+    name: "缓冲运动碰壁",
   },
   {
-    url: 'http://49.234.210.170/js/回到首页，触底.html',
-    name: '回到首页，触底',
+    url: "http://49.234.210.170/js/回到首页，触底.html",
+    name: "回到首页，触底",
   },
   {
-    url: 'http://49.234.210.170/js/轮播图.html',
-    name: '轮播图',
+    url: "http://49.234.210.170/js/轮播图.html",
+    name: "轮播图",
   },
   {
-    url: 'http://49.234.210.170/js/碰撞.html',
-    name: '碰撞',
+    url: "http://49.234.210.170/js/碰撞.html",
+    name: "碰撞",
   },
   {
-    url: 'http://49.234.210.170/js/拖动元素四边距.html',
-    name: '拖动元素四边距',
+    url: "http://49.234.210.170/js/拖动元素四边距.html",
+    name: "拖动元素四边距",
   },
   {
-    url: 'https://blog.csdn.net/qq_42877027/article/details/104959320',
-    name: 'ajax',
+    url: "https://blog.csdn.net/qq_42877027/article/details/104959320",
+    name: "ajax",
   },
   {
-    url: 'http://49.234.210.170/js/cookies.html',
-    name: 'cookies',
+    url: "http://49.234.210.170/js/cookies.html",
+    name: "cookies",
   },
   {
-    url: 'http://49.234.210.170/js/jsonp.html',
-    name: 'jsonp',
+    url: "http://49.234.210.170/js/jsonp.html",
+    name: "jsonp",
   },
   {
-    url: 'http://49.234.210.170/js/Magnifier/放大镜.html',
-    name: '放大镜',
+    url: "http://49.234.210.170/js/Magnifier/放大镜.html",
+    name: "放大镜",
   },
   {
-    url: 'https://blog.csdn.net/qq_42877027/article/details/104959936',
-    name: 'node开启Http',
+    url: "https://blog.csdn.net/qq_42877027/article/details/104959936",
+    name: "node开启Http",
   },
   {
-    url: 'http://49.234.210.170/js/购物车/todopst/todo.html',
-    name: 'todopst',
+    url: "http://49.234.210.170/js/购物车/todopst/todo.html",
+    name: "todopst",
   },
   {
-    url: 'http://49.234.210.170/js/懒加载瀑布流/图片懒加载.html',
-    name: '图片懒加载',
+    url: "http://49.234.210.170/js/懒加载瀑布流/图片懒加载.html",
+    name: "图片懒加载",
   },
   {
-    url: 'http://49.234.210.170/js/日历.html',
-    name: '日历',
+    url: "http://49.234.210.170/js/日历.html",
+    name: "日历",
   },
   {
-    url: 'http://49.234.210.170/js/评分效果.html',
-    name: '评分效果',
+    url: "http://49.234.210.170/js/评分效果.html",
+    name: "评分效果",
   },
   {
-    url: 'http://49.234.210.170/js/懒加载瀑布流/瀑布流.html',
-    name: '瀑布流',
+    url: "http://49.234.210.170/js/懒加载瀑布流/瀑布流.html",
+    name: "瀑布流",
   },
   {
-    url: 'http://49.234.210.170/js/mobileApi.html',
-    name: 'mobileApi',
+    url: "http://49.234.210.170/js/mobileApi.html",
+    name: "mobileApi",
   },
   {
-    url: 'http://49.234.210.170/js/排序算法.html',
-    name: '排序算法',
-    icon: <FireFilled style={{ color: '#FF4500' }} />,
+    url: "http://49.234.210.170/js/排序算法.html",
+    name: "排序算法",
+    icon: <FireFilled style={{ color: "#FF4500" }} />,
   },
   {
-    url: 'http://49.234.210.170/js/双飞翼布局.html',
-    name: '双飞翼布局',
+    url: "http://49.234.210.170/js/双飞翼布局.html",
+    name: "双飞翼布局",
   },
   {
-    url: 'http://49.234.210.170/js/复利计算.html',
-    name: '复利计算',
-    icon: <FireFilled style={{ color: '#FF4500' }} />,
-  },
-
-  {
-    url: 'http://49.234.210.170/js/滚动.html',
-    name: '滚动',
+    url: "http://49.234.210.170/js/复利计算.html",
+    name: "复利计算",
+    icon: <FireFilled style={{ color: "#FF4500" }} />,
   },
 
   {
-    url: 'http://49.234.210.170/js/video.html',
-    name: 'video',
+    url: "http://49.234.210.170/js/滚动.html",
+    name: "滚动",
   },
 
   {
-    url: 'http://49.234.210.170/js/横屏video.html',
-    name: '横屏video',
+    url: "http://49.234.210.170/js/video.html",
+    name: "video",
+  },
+
+  {
+    url: "http://49.234.210.170/js/横屏video.html",
+    name: "横屏video",
   },
 ];
 
 let outNum = 1;
 
 export default function Home() {
-  const [autorColor, setAutorColor] = useState('red');
+  let num = 1;
+
+  const [autorColor, setAutorColor] = useState("red");
   const [pageStyle, setPageStyle] = useState({
     opacity: 0,
   });
@@ -128,7 +130,7 @@ export default function Home() {
       speedX: 1,
       speedY: 1,
       ms: 20,
-      className: '.' + styles.ewm,
+      className: "." + styles.ewm,
     });
 
     // 页面自动展示计时器
@@ -137,12 +139,17 @@ export default function Home() {
     }, 1000);
 
     // 判断设备类型
+    if (num === 1) {
+    }
+
+    num = num + 1;
 
     // 卸载时
     return () => {
       clearInterval(colorTimer);
       clearTimeout(pageTimer);
     };
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -165,19 +172,19 @@ export default function Home() {
       </Head>
       {/* Page Start */}
       <div
-        className={`${styles['container']} ${styles['theme-gray']}`}
+        className={`${styles["container"]} ${styles["theme-gray"]}`}
         onClick={onPageClick}
       >
         {/* 视频 */}
         <VideoBG />
-        <div className={styles['page']} style={pageStyle}>
+        <div className={styles["page"]} style={pageStyle}>
           <div className={styles.header}></div>
           <div className={styles.content}>
             <h1 className={styles.title}>藏轮阁</h1>
             <div className={styles.author} style={autorColorStyle}>
               辛望 (Simon)
             </div>
-            <div className={styles['demo-list']}>
+            <div className={styles["demo-list"]}>
               {BlogUrlList.map((item, index) => {
                 return (
                   <p className={styles.link} key={index}>
@@ -192,7 +199,7 @@ export default function Home() {
           </div>
           <div className={styles.ewm}>
             <Image
-              className={styles['ewm-img']}
+              className={styles["ewm-img"]}
               width={100}
               height={100}
               alt="二维码"
