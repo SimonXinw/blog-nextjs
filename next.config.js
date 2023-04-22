d2db44492f6266808f71623c2460a9fed9c01251
辛望 <1294600058@qq.com>
2023-04-22 21:30:12 +0800

    feat: 恢复编译

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.xinwangblog.cn",
-        port: "443",
+        port: "",
         pathname: "/**",
       },
     ],
