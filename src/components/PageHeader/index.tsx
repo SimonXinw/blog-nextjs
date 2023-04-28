1f6aaa99e2fde97a2de89f7f2258f85da0202a53
wb-xw818042 <wb-xw818042@alibaba-inc.com>
2023-04-28 18:34:44 +0800

    feat: 增加图标

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BarChartOutlined, UserOutlined, BugOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
+import styles from './index.module.css';
+import Link from 'next/link';
+
+const PageHeader: React.FC = () => {
+  return (
+    <nav className={styles['header']}>
+      <Space size={[24, 0]} wrap align="center">
+        <BarChartOutlined style={{ fontSize: '24px' }} />
+        <Link href="/admin/craw">
+          <BugOutlined style={{ fontSize: '24px' }} />
+        </Link>
+        <Link href="/admin">
+          <UserOutlined style={{ fontSize: '24px' }} />
+        </Link>
+      </Space>
+    </nav>
+  );
+};
+
+export default PageHeader;
---
+++ src/components/index.tsx
@@ -0,0 +1,6 @@
+import EWM from './EWM';
+import PageHeader from './PageHeader';
+import RecordNumber from './RecordNumber';
+import VideoBG from './VideoBG';
+
+export { EWM, PageHeader, RecordNumber, VideoBG };
