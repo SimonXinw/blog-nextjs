741aa01d001c8da2a91537523790bf0491a4ccfb
xinwang <1294600058@qq.com>
2024-12-07 18:33:14 +0800

    feat: 多语言3国家文本成功

--- app/(site)/[locale]/product/page.tsx
+++ app/(site)/[locale]/product/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { i18n, LocaleType } from "i18n/index";
 import { cookies } from "next/headers"; // 用于在服务端访问 cookie
+import { NextRequest } from "next/server"; // 获取路由信息
 
-export default async function Page() {
-  // 获取请求中的 cookies
-  const cookieStore = await cookies();
-
-  const locale: LocaleType =
-    (cookieStore.get("NEXT_LOCALE")?.value as LocaleType) || "zh"; // 获取 NEXT_LOCALE cookie
+export default async function Page({
+  params,
+}: {
+  params: { locale: LocaleType };
+}) {
+  // 这里的 params.locale 会直接从动态路由参数获取到
+  const { locale } = params;
 
   // 获取对应语言字典
   const langDict: Record<string, string> = await i18n.init(locale);
