463ac7e974396c816cf8498e3adad30915fcd4dc
xinwang <1294600058@qq.com>
2024-12-09 17:57:09 +0800

    TODO: 增加了一个 supabase 数据库，之后就用这个数据库了，很好用

--- app/(site)/[locale]/product/page.tsx
+++ app/(site)/[locale]/product/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { i18n, LocaleType } from "i18n/index";
+import supabase from "lib/supabase";
+
+const getData = async () => {
+  const { data, error } = await supabase.from("user").select("*");
+  if (error) {
+    return { success: false, error: error.message };
+  }
+
+  return data;
+};
 
 export default async function Page({
   params,
@@ -8,12 +18,15 @@ export default async function Page({
 }) {
   const { locale } = await params;
 
+  const data = await getData();
+
   // 获取对应语言字典
   const langDict: Record<string, string> = await i18n.init(locale);
 
   return (
     <div className="flex justify-center items-center h-screen bg-black text-white">
       {langDict.product}
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 }
