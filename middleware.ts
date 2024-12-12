f59b448f86c17141cd319974e7088f2e3c293087
xinwang <1294600058@qq.com>
2024-12-12 12:59:28 +0800

    feat: 放开限制 logo  public

--- middleware.ts
+++ middleware.ts
@@ -130,6 +130,6 @@ export async function middleware(request: NextRequest & { ip: string }) {
  */
 export const config = {
   matcher: [
-    "/((?!api|public|_next/static|_next/image|favicon.ico|images|login|register|fonts).*)",
+    "/((?!api|public|_next/static|_next/image|favicon.ico|images|login|register|fonts|logo|videos).*)",
   ],
 };
